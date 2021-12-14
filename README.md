0ddb14838c685fadb3e0642064efa1ea3ab43858
Emerson Menezes <emerson.menezes@modalgr.com.br>
2021-12-14 11:07:47 -0300

    Gerando codigos aleatorios e mostrando no html

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # container-number-generator-ISO6346
 Gera números válidos de Containers segundo a norma ISO 6346
+
+[ISO-6346 Wikipedia](https://en.wikipedia.org/wiki/ISO_6346)
--- valida-container.js
+++ valida-container.js
@@ -1,63 +1,80 @@
-
-const alphabet = {
-    A: 10,
-    B: 12,
-    C: 13,
-    D: 14,
-    E: 15,
-    F: 16,
-    G: 17,
-    H: 18,
-    I: 19,
-    J: 20,
-    K: 21,
-    L: 23,
-    M: 24,
-    N: 25,
-    O: 26,
-    P: 27,
-    Q: 28,
-    R: 29,
-    S: 30,
-    T: 31,
-    U: 32,
-    V: 34,
-    W: 35,
-    X: 36,
-    Y: 37,
-    Z: 38,
-    0: 0,
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8,
-    9: 9,
-};
-
-
-let numeroContainer = [
-    // Digite o número do container o qual será gerado o dígito verificador
-    "AAAA000000",
-    "AAAA000001",
-    "AAAA000002",
-    "AAAA000003",
-    "AAAA000004",
-    "AAAA000005",
-]
-
-let somaTotal = numeroContainer.map(c => c.split('')
-    .map(n=> alphabet[n])
-    .map((n,i) => n*(2**i))
-    .reduce((a,b) => a+b)
-)
-
-let digitoVerificador = somaTotal.map(n => n % 11)
+function gerarCodigos() {
+    const alphabet = {
+        A: 10,
+        B: 12,
+        C: 13,
+        D: 14,
+        E: 15,
+        F: 16,
+        G: 17,
+        H: 18,
+        I: 19,
+        J: 20,
+        K: 21,
+        L: 23,
+        M: 24,
+        N: 25,
+        O: 26,
+        P: 27,
+        Q: 28,
+        R: 29,
+        S: 30,
+        T: 31,
+        U: 32,
+        V: 34,
+        W: 35,
+        X: 36,
+        Y: 37,
+        Z: 38,
+        0: 0,
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+    };
 
 
-numeroContainer.map((a,i) => {
-    console.log(a+digitoVerificador[i])
-})
+    let quantidadeCodigos = document.getElementById("qtdCodigos").value
+    let codigos = []
+
+    for (let i = 0; i < quantidadeCodigos; i++) {
+        let alg = ''
+
+        for (let j = 0; j < 3; j++) {
+            let alphabetAux = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            let a = alphabetAux.charAt(Math.floor(Math.random() * alphabetAux.length))
+            alg += a
+        }
+
+        let lastChars = 'JUZ'
+        let b = lastChars.charAt(Math.floor(Math.random() * lastChars.length))
+        alg += b
+
+        for (let j = 0; j < 6; j++) {
+            let numbers = '0123456789'
+            let c = numbers.charAt(Math.floor(Math.random() * numbers.length))
+            alg += c
+        }
+        codigos.push(alg)
+    }
+
+    let somaTotal = codigos.map(c => c.split('')
+        .map(n => alphabet[n])
+        .map((n, i) => n * (2 ** i))
+        .reduce((a, b) => a + b)
+    )
+
+    let digitoVerificador = somaTotal.map(n => n % 11)
+
+    codigos.map((a, i) => {
+        digitoVerificador[i] = digitoVerificador[i] == 10 ? 0 : digitoVerificador[i]
+        let r = a + digitoVerificador[i]
+        console.log(r)
+        document.getElementById("result").innerText += ` ${r} | `
+    })
+}
